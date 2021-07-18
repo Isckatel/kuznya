08c0da324aa869686a6cb1626334eda40c91621a
Dom\Ivan <nt-polza@yandex.ru>
2021-07-18 10:13:23 +0500

    Бургер и немного jQ

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,6 @@
 console.log('Скрипты подключены!');
- 
+$(document).ready(function() {
+  $('.burger').click(function(event){
+    $('.burger, .mainMenu').toggleClass('active');//нажали добавили класс, нажали еще раз убрали класс
+  })  
+})
