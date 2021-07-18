317c288b9e159bad175b463287a9bb14caf495fd
Dom\Ivan <nt-polza@yandex.ru>
2021-07-18 13:50:06 +0500

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,7 @@ console.log('Скрипты подключены!');
 $(document).ready(function() {
   $('.burger').click(function(event){
     $('.burger, .mainMenu').toggleClass('active');//нажали добавили класс, нажали еще раз убрали класс
-  })  
+    //блокируем прокрутку страницы
+    $('body').toggleClass('lock');
+  })
 })
